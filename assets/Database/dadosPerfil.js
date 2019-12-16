63bdf55b8428c84383cc74ccb70a1224735fd3f3
BrenoMaia98 <brenomaia98@gmail.com>
2019-12-15 21:34:11 -0300

    adicionado o modal de refeição base e seu funcionamento, faltando somente confirmar a seleção de uma refeição e adicionar os alimentos aos já selecionados no registro

---
+++ assets/Database/dadosPerfil.js
@@ -0,0 +1,16 @@
+
+import { Alimentos } from "./alimentos";
+
+export const reqDadosPerfil = {
+    reqRemedios: [
+        { nome: "medicamento 1", vezes: 1, mg: 45 },
+        { nome: "medicamento 2", vezes: 2, mg: 20 },
+        { nome: "medicamento 3", vezes: 1, mg: 75 },
+        { nome: "medicamento 4", vezes: 3, mg: 30 },
+    ],
+    reqAlimentos: Alimentos,
+    hiperglicemia: 140,
+    maxIdeal: 120,
+    minIdeal: 80,
+    hipoglicemia: 60,
+}
---
+++ assets/Database/refeicao.js
@@ -0,0 +1,43 @@
+
+export const RefeicoesBase = [
+    {
+        apelido : "churrasco",
+        alimentos : [
+            "Pão francês",
+            "Carne de boi picanha (Friboi)",
+            "Carne de vaca maminha",
+            "Mandioca cozida (picada)","Pão francês",
+            "Carne de boi picanha (Friboi)",
+            "Carne de vaca maminha",
+            "Mandioca cozida (picada)","Pão francês",
+            "Carne de boi picanha (Friboi)",
+            "Carne de vaca maminha",
+            "Mandioca cozida (picada)","Pão francês",
+            "Carne de boi picanha (Friboi)",
+            "Carne de vaca maminha",
+            "Mandioca cozida (picada)","Pão francês",
+            "Carne de boi picanha (Friboi)",
+            "Carne de vaca maminha",
+            "Mandioca cozida (picada)","Pão francês",
+            "Carne de boi picanha (Friboi)",
+            "Carne de vaca maminha",
+            "Mandioca cozida (picada)","Pão francês",
+            "Carne de boi picanha (Friboi)",
+            "Carne de vaca maminha",
+            "Mandioca cozida (picada)","Pão francês",
+            "Carne de boi picanha (Friboi)",
+            "Carne de vaca maminha",
+            "Mandioca cozida (picada)",
+        ]
+    },
+    {
+        apelido : "almoço simples",
+        alimentos : [
+            "Arroz branco cozido",
+            "Bife de boi",
+            "Feijão carioquinha cozido",
+            "Alface americana crua",
+            "Farofa",
+        ]
+    },
+]
